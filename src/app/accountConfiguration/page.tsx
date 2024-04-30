5a00144fa82b0033f9fbe8b8b934e77255242c29
Rebekah Gulliver <rebekah.gulliver@fujitsu.com>
2024-04-30 23:32:11 +0100

    account config content update

--- src/app/accountConfiguration/page.tsx
+++ src/app/accountConfiguration/page.tsx
@@ -1,99 +1,45 @@
 import React from 'react';
 import Navbar from "../../components/navigation-bar/navigation";
-import styles from "../accountConfiguration/USER-home.module.css"; 
+import styles from "../accountConfiguration/accountConfiguration.module.css"; 
+import Link from 'next/link';
 
-
-  export function Page (){
+export function Page (){
   return (
-    <div className={styles.container}>
+    <div>
+      <Navbar/>
+      <div className={styles.container}>
 
-    <div><Navbar/></div>
-    <div className={styles.topSection}>
-      <div className={styles.fullWidthContainer}>
-        <h2>KnowledgeBase Onboarding</h2>
-      </div>
-      <div className={styles.fullWidthContainer}>
-        <button>Version History</button>
-        <button>Edit Content</button>
-      </div>
-    </div>
-
-    <div className={styles.myTeamSection}>
-      <div className={styles.fullWidthContainer}>
-        <div className={styles.topSectionContainer}>
-          <h2 className={styles.teamTitle}>My Team</h2>
-          <button className={styles.teamButton}>Add New Member</button>
-        </div>
-      </div>
-      <div className={styles.fullWidthContainer}>
-        <div className={styles.listContainer}>
-          <ul className={styles.userList}>
-            <li className={styles.userListItem}>
-              <span className={styles.userName}>John Doe</span>
-              <div className={styles.progressBarContainer}>
-                <progress className={styles.progress} value="60" max="100"></progress>
-              </div>
-              <div className={styles.reminderButtonContainer}>
-                <button className={styles.reminderButton}>Send Reminder</button>
-              </div>
-            </li>
-            <li className={styles.userListItem}>
-              <span className={styles.userName}>Jane Doe</span>
-              <div className={styles.progressBarContainer}>
-                <progress className={styles.progress} value="20" max="100"></progress>
-              </div>
-              <div className={styles.reminderButtonContainer}>
-                <button className={styles.reminderButton}>Send Reminder</button>
-              </div>
-            </li>
-            <li className={styles.userListItem}>
-              <span className={styles.userName}>Chris Lee</span>
-              <div className={styles.progressBarContainer}>
-                <progress className={styles.progress} value="60" max="100"></progress>
-              </div>
-              <div className={styles.reminderButtonContainer}>
-                <button className={styles.reminderButton}>Send Reminder</button>
-              </div>
-            </li>
-            <li className={styles.userListItem}>
-              <span className={styles.userName}>Morgan Smith</span>
-              <div className={styles.progressBarContainer}>
-                <progress className={styles.progress} value="100" max="100"></progress>
-              </div>
-              <div className={styles.reminderButtonContainer}>
-                <button className={styles.reminderButton}>Send Reminder</button>
-              </div>
-            </li>
-            <li className={styles.userListItem}>
-              <span className={styles.userName}>Kelly Wright</span>
-              <div className={styles.progressBarContainer}>
-                <progress className={styles.progress} value="80" max="100"></progress>
-              </div>
-              <div className={styles.reminderButtonContainer}>
-                <button className={styles.reminderButton}>Send Reminder</button>
-              </div>
-            </li>
-            <li className={styles.userListItem}>
-              <span className={styles.userName}>Mark Cross</span>
-              <div className={styles.progressBarContainer}>
-                <progress className={styles.progress} value="55" max="100"></progress>
-              </div>
-              <div className={styles.completedTextContainer}>
-                <span className={styles.completedText}>Completed 14/01/2024</span>
-              </div>
-            </li>
+        <div className={styles.accountSection}>
+          <h2 className={styles.h2}>Account Configuration</h2>
+          <ul className={styles.accountList}>
+            <li>Ensure ability to disable the forcepoint proxy Ask IT Service Catalog - Ask IT Portal (service-now.com)</li>
+            <li>Ensure you have admin access for installations NetScaler AAA</li>
+            <li>Turn off GlobalProtect VPN: once turned off from the desktop icon, open your services as an administrator and make sure PanGPS is not running, with startup type set to manual</li>
+            <li>If your line manager is out-of-office, request the 3 day admin access as this does not require approval</li>
+            <li>If your line manager is available, request the 1 year admin access and message them to prompt them to approve</li>
+            <li>Byte Guard account (work with admin in team to set up account and 2FA)</li>
+            <li>Google Authenticator App (used to generate one-time password, links to Byte Guard)</li>
+            <li>AWS CLI</li>
+            <li>Node.js:  (instructions in README)</li>
+            <li>ESLint: (instructions in README)</li>
+            <li>Git</li>
+            <li>Github Desktop (optional)</li>
+            <li>VS Code</li>
+            <li>Terraform (instructions in README)</li>
+            <li>Chocolatey (optional)  (instructions in README)</li>
+            <li>Python3 </li>
+            <li>Mongosh (instructions in README)</li>
+            <li>Access to ADO platform (Tech Lead to set up)</li>
+            <li>Designs: Figma File</li>
+            <li>Review the Architecture Overview Document</li>
           </ul>
         </div>
+        <div className={styles.nextButtonContainer}>
+        <button className={styles.nextButton}><Link href="/knowledgeCheck2">Next</Link></button>
+      </div>
       </div>
     </div>
-
-
-
-
-  </div>
   );
 };
 
 export default Page;
-
-
